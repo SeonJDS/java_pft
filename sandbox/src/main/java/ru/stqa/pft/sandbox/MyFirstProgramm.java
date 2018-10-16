4b1d6da619291528f52f0bfab7082fa6c588df05
seon1122 <vladdpua7@gmail.com>
2018-10-15 22:39:54 -0400

    Добавлено несколко функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -4,6 +4,25 @@ public class MyFirstProgramm {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        hello("Vasya!");
+
+        double l = 5;
+        System.out.println("Area of a square with a side that euqals: " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Area of a rectangle with sides that are equl: " + a + " & " + b + " = " + area(a, b));
+    }
+
+    public static void  hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static double  area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
