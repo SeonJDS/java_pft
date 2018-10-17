6a6338cf1526e64da5b29ddc9d1711f797478958
seon1122 <vladdpua7@gmail.com>
2018-10-17 00:16:56 -0400

    Testng подключен к sandbox. Добавлен класс PointTest

---
+++ sandbox/src/test/java/ru/stqa/pft/sanbox/PointTest.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sanbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Point;
+
+public class PointTest {
+
+    @Test
+    public void verifyDistanceIsEqual() { //проверка, что расстояние между точками больше 0
+        Point p1 = new Point(4, 2);
+        Point p2 = new Point(5, 3);
+        Assert.assertTrue(p1.distance(p2) > 0);
+    }
+
+    @Test
+    public void verifyNumberIsCorrect() { //проверка, что число возвращаемое методом distance равно числу, которое было найденно на сайте, где находится расстояние между точками
+        Point p1 = new Point(4, 2);
+        Point p2 = new Point(5, 3);
+        Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
+    }
+}
