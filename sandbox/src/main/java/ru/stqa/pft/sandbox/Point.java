18e475d842f0435c2a10c8ad4de93f32ddc18b47
seon1122 <vladdpua7@gmail.com>
2018-10-16 22:23:51 -0400

    Метод distance с параметром Point p2 добавлен в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.PointsHomeWork.square;
+
 public class Point {
 
     public double a;
@@ -10,5 +12,12 @@ public class Point {
         this.b = b;
     }
 
+    public double distance(Point p2) { //метод рассчитывающий дистанцию между двумя точками
+        return Math.sqrt(squareX(p2.a, this.a) + squareX(p2.b, this.b));
+    }
+
+    public static double squareX(double a, double b) { //функция расччитывающая квадрат разницы двух чисел
+        return (a * a) - 2 * a * b + (b * b);
+    }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointsHomeWork.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointsHomeWork.java
@@ -7,14 +7,17 @@ public class PointsHomeWork {
         Point p1 = new Point(1.0, 3.0);
         Point p2 = new Point(2.0, 5.0);
 
-        System.out.println("Distance between point p1 " + p1.a + ", " + p1.b + " & point p2 " + p2.a + ", " + p2.b  + " = " + distance(p1, p2));
+        System.out.println("Distance between point p1 " + p1.a + ", " + p1.b + " & point p2 " + p2.a + ", " + p2.b  + " = " + distance(p1, p2));//вывод на экран результат действия функции distance описанной вутри класса PointHomework
+
+        System.out.println("Distance between point p1 " + p1.a + ", " + p1.b + " & point p2 " + p2.a + ", " + p2.b  + " = " + p1.distance(p2));//вывод на экран результат действия функции distance описанной вутри класса Point
+
     }
 
-    public static double distance(Point p1, Point p2) { //метод рассчитывающий разницу двух точек
+    public static double distance(Point p1, Point p2) { //функция рассчитывающая дистанцию между двумя точками
         return Math.sqrt(square(p2.a, p1.a) + square(p2.b, p1.b));
     }
 
-    public static double square(double a, double b) { //Метод расччитывающий квадрат разницы двух чисел
+    public static double square(double a, double b) { //функция расччитывающая квадрат разницы двух чисел
         return (a * a) - 2 * a * b + (b * b);
     }
 }
