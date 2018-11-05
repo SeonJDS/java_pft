e6bc25b514b4d6887d5dad1fd6429ce9df053ab1
seon1122 <vladdpua7@gmail.com>
2018-11-04 22:10:39 -0500

    Добавил проверку наличия группы перед созданием нового контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactCreationTests.java
@@ -2,11 +2,17 @@ package ru.stqa.pft.adressbook.tests;
 
 import org.testng.annotations.*;
 import ru.stqa.pft.adressbook.model.ContactData;
+import ru.stqa.pft.adressbook.model.GroupData;
 
 public class ContactCreationTests extends TestBase{
 
   @Test
   public void testContactCreation() {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      GroupData group = new GroupData("test1", null, null);
+      app.getGroupHelper().createGroup(group);
+    }
     app.getContactHelper().createContact(new ContactData("test1", "test2", "1234567890", "test@test.com", "test1"), true);
   }
 }
