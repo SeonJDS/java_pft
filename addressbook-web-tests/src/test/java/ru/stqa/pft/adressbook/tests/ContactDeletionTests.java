6184791accafc803654ee6d5ba5352797fbb7c39
seon1122 <vladdpua7@gmail.com>
2018-11-11 23:22:51 -0500

    Лекция 5.1. Как правильно запускать браузер

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactDeletionTests.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
-    @Test
+    @Test (enabled = false)
     public void testContactDeletion() {
         if(! app.getContactHelper().isThereAContact()) { //проверка наличия контакта
             app.getNavigationHelper().gotoGroupPage();
