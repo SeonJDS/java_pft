b45143dd6412482d4e40befe61e947dc546ba251
seon1122 <vladdpua7@gmail.com>
2018-11-18 23:31:03 -0500

    Лекция 6.6. Формат XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/model/GroupData.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.adressbook.model;
 
-public class GroupData {
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
+@XStreamAlias("groups")
+
+public class GroupData {
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;;
     private String name;
     private String header;
