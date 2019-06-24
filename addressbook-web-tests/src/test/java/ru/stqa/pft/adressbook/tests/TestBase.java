b9ada03628439abd9ac82991661f87dd6208bb70
Vladyslav Korol <vladyslavkorol@Vladyslavs-MacBook-Pro.local>
2019-06-23 23:05:09 -0400

    Commiting latest changes to address book project.
    Changes to addressbook project as well as adding new project to the repository.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -20,7 +20,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
-    Logger logger = LoggerFactory.getLogger(TestBase.class);
+    //Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -34,7 +34,7 @@ public class TestBase {
         app.stop();
     }
 
-    @BeforeMethod
+    /*@BeforeMethod
     public void logTestStart(Method m, Object[] p) {
         logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
     }
@@ -42,7 +42,7 @@ public class TestBase {
     @AfterMethod(alwaysRun = true)
     public void logTestStop(Method m) {
         logger.info("Stop test " + m.getName());
-    }
+    }*/
 
     public void verifyGroupListInUI() {
         if (Boolean.getBoolean("verifyUI")) {
